cb9498a3400b88d3822d6568407759df6f3a2ebf
Mahadeo Kr <mahadeo.t@bigdoorsolutions.co.in>
2024-09-16 15:51:27 +0530

    home page

---
+++ src/lib/carddata.js
@@ -0,0 +1,50 @@
+// data.js or a similar file
+import img1 from '../assets/Frame 145.png';
+import img2 from '../assets/Frame 146.png';
+import img3 from '../assets/Frame 147.png';
+import img4 from '../assets/Frame 148.png';
+import img5 from '../assets/Frame 149.png';
+import img6 from '../assets/Frame 150.png';
+
+
+const cardData = [
+    {
+        imgSrc: img1,
+        heading: 'IT Training & Internship Programs',
+        paragraph: 'We aim to create a space where curiosity about technology grows without the fear of staying behind in race. With our expertise, we are dedicated to analyzing upcoming trends and embracing new technologies with agility.',
+        bgColor: 'bg-lightpurple'
+    },
+    {
+        imgSrc: img2,
+        heading: 'Enterprise App Development  (Mobile & Web)',
+        paragraph: 'We aim to create a space where curiosity about technology grows without the fear of staying behind in race. With our expertise, we are dedicated to analyzing upcoming trends and embracing new technologies with agility.',
+        bgColor: 'bg-lightgreen'
+    },
+    {
+        imgSrc: img3,
+        heading: 'Process Training & Certifications',
+        paragraph: 'We aim to create a space where curiosity about technology grows without the fear of staying behind in race. With our expertise, we are dedicated to analyzing upcoming trends and embracing new technologies with agility.',
+        bgColor: 'bg-lightorange'
+    },
+    {
+        imgSrc: img4,
+        heading: 'Digital Marketing',
+        paragraph:'We aim to create a space where curiosity about technology grows without the fear of staying behind in race. With our expertise, we are dedicated to analyzing upcoming trends and embracing new technologies with agility.',
+        bgColor: 'bg-lightbrown'
+    },
+    {
+        imgSrc: img5,
+        heading: 'Managed Services & Placement Support',
+        paragraph: 'We aim to create a space where curiosity about technology grows without the fear of staying behind in race. With our expertise, we are dedicated to analyzing upcoming trends and embracing new technologies with agility.',
+        bgColor: 'bg-lightgrey'
+    },
+    {
+        imgSrc: img6,
+        heading: 'Career Guidance & Professional Wellbeing',
+        paragraph: 'We aim to create a space where curiosity about technology grows without the fear of staying behind in race. With our expertise, we are dedicated to analyzing upcoming trends and embracing new technologies with agility.',
+        bgColor: 'bg-lightpurple'
+    },
+    // Add more card data here
+];
+
+export default cardData;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ export default {
         doggerblue:'#2E89E5',//Add second button bg
         DavyGrey: '#5A5A5A', //Add heading colour
         black:'#1D2026', //Add footer bg
+        lightpurple:'#EBEBFF',
+        lightgreen:'#E1F7E3',
+        lightorange:'#FFF2E5',
+        lightbrown:'#FFF0F0',
+        lightgrey:'#F5F7FA',
+        
+
       },
       fontFamily: {
         sora:['Sora','sans-serif'],
